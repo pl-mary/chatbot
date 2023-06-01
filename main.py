13d8658e72ab84252a05b85b994823b07ba300c8
Maria Pletneva <pl-mary99@yandex.ru>
2023-06-01 20:59:39 +0300

    feat(#3): add classification message

--- main.py
+++ main.py
@@ -6,13 +6,30 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from dotenv import dotenv_values
 
-API_TOKEN = dotenv_values(".env").get('BOT_TOKEN')
+API_TOKEN = "6229195886:AAFgI4TsQqEiiIaLwvdAWaIdax6BqNoCekQ"
 
 logging.basicConfig(level=logging.INFO)
 
 storage = MemoryStorage()
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=storage)
+
+
+def messageIMT(imt):
+    if imt < 16:
+        return 'Выраженный дефицит массы тела'
+    elif 16 <= imt < 18.5:
+        return 'Недостаточная масса тела'
+    elif 18.5 <= imt < 25:
+        return 'Нормальная масса тела'
+    elif 25 <= imt < 30:
+        return 'Избыточная масса тела (предожирение)'
+    elif 30 <= imt < 35:
+        return 'Ожирение 1-ой степени'
+    elif 35 <= imt <= 40:
+        return 'Ожирение 2-ой степени'
+    else:
+        return 'Ожирение 3-ой степени'
 
 
 class States(StatesGroup):
@@ -43,8 +60,12 @@ async def load_weight(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         if message.text.isdigit():
             data['weight'] = message.text
-            await bot.send_message(chat_id=message.from_user.id,
-                                   text=f"Ваш ИМТ: {int(data['weight']) / (int(data['height']) * int(data['height']) * 0.0001)}")
+            height = float(data['height'])
+            weight = float(data['weight'])
+            imt = weight / (height * height * 0.0001)
+            await bot.send_message(chat_id=message.from_user.id, text=f"Ваш ИМТ: {imt}")
+            await bot.send_message(chat_id=message.from_user.id, text=messageIMT(imt))
+
         else:
             await message.answer("Только числа! Теперь начинай заново...")
         await state.finish()
