3b9c2d697f3a40f5be16d18da23250792747e885
Elza Gazizova <elza396@yandex.ru>
2023-06-01 21:14:28 +0500

    feat(#2): add template for chatbot

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+import logging
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from dotenv import dotenv_values
+
+API_TOKEN = dotenv_values(".env").get('BOT_TOKEN')
+
+logging.basicConfig(level=logging.INFO)
+
+storage = MemoryStorage()
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot, storage=storage)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class States(StatesGroup):
+    height = State()
+    weight = State()
 
 
-# Press the green button in the gutter to run the script.
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer("Введите рост!")
+    await States.height.set()
+
+
+@dp.message_handler(state=States.height)
+async def load_height(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        if message.text.isdigit():
+            data['height'] = message.text
+            await message.answer("Теперь введите вес!")
+            await States.weight.set()
+        else:
+            await message.answer("Только числа! Теперь начинай заново...")
+            await state.finish()
+
+
+@dp.message_handler(state=States.weight)
+async def load_weight(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        if message.text.isdigit():
+            data['weight'] = message.text
+            await bot.send_message(chat_id=message.from_user.id,
+                                   text=f"Ваш ИМТ: {int(data['weight']) / (int(data['height']) * int(data['height']) * 0.0001)}")
+        else:
+            await message.answer("Только числа! Теперь начинай заново...")
+        await state.finish()
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer("Начните с команды /start")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    executor.start_polling(dp, skip_updates=True)
